2f31c19198e308b31ae5b36dd15960dffe2a5233
junhyeong9812 <pickjog@gmail.com>
2026-01-15 12:07:00 +0900

    chore: 프로젝트 초기화 및 설계

---
+++ src/main/java/com/payroll/calculator/PayrollCalculatorApplication.java
@@ -0,0 +1,12 @@
+package com.payroll.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayrollCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayrollCalculatorApplication.class, args);
+    }
+}
